82bbc5a0cc8e97676b2aaa089afdf97ee4532b6d
sim1222 <kokt@sim1222.com>
2022-03-29 13:21:20 +0900

    first commit

---
+++ TurnOffDispCs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.InteropServices;
+
+class Win32API
+{
+    [DllImport("user32.dll")]
+    public static extern bool PostMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetForegroundWindow();
+}
+
+
+namespace TurnOffDisp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Trying turn off screens...");
+                Task.Delay(1000).Wait();
+                Win32API.PostMessage(Win32API.GetForegroundWindow(), 274, 61808, 2);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e}");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
